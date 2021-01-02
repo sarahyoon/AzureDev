dd9966374c8714a7015761d0898b0e637a2fd826
sarahyoon <yoonsarah88@gmail.com>
2021-01-02 20:37:45 +0900

    edit port number

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from FlaskWebProject import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', '52.188.127.125')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '8080'))
     except ValueError:
-        PORT = 5555
+        PORT = 8080
     app.run(HOST, PORT, ssl_context='adhoc')
