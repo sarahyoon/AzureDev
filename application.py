733acc891a78c9f975859e23efe50823d121c835
sarahyoon <yoonsarah88@gmail.com>
2021-01-02 20:56:31 +0900

    edit server_hos

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ from os import environ
 from FlaskWebProject import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', '52.188.127.125')
+    HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
